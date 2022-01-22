0021120e6bc430de6b58ff230fa446ba83f56e41
David_CB <cruzdavid158@gmail.com>
2022-01-21 19:09:00 -0600

    update header.php

--- partials/header.php
+++ partials/header.php
@@ -1,26 +1,43 @@
-<header class="header" id="header">
- <div class="logo">
-   <img id="logo" src="images/Cuatimar_logo.png" alt="Logo Cuatimar"  onclick="location.href='index'">
- </div>
-  <div class="header_redes">
-   <div class="redes_nav"><a href="https://www.youtube.com/channel/UCrH-GU10xq1IQShqwHs29fg" target="blank"><i class="fab fa-youtube fa-lg"></i></a></div>
-   <div class="redes_nav"><a href="https://www.linkedin.com/company/cuatimar-construcciones" target="blank"><i class="fab fa-linkedin fa-lg"></i></a></div>
-   <div class="redes_nav"><a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank"><i class="fas fa-at fa-lg"></i></a></div>
-   <div class="redes_nav"><a href="tel:+527225443902"><i class="fas fa-phone-square-alt fa-lg"></i></a></div>
- </div>
- <nav>
-   <ul>
+<header>
+    <div class="logo">
+      <a href="index"><img class="logo_img" src="images/Cuatimar_logo.png" alt="Logo Cuatimar"></a>
+    </div>
 
-     <li class="lihov submenu">
-       <a id="enlace-inicio" class="navbar" href="index">Nosotros</a>
-       <ul class="children ulhov">
-         <li class="lihov">
-           <a class="navbar" href="Curriculum_Cuatimar.pdf" target="_blank">Curriculum</a>
-         </li>
-       </ul>
-     </li>
-     <li class="lihov"><a class="navbar" href="Servicios">Servicios</a></li>
-     <li class="lihov"><a class="navbar" href="#Contacto">Contacto</a></li>
-   </ul>
- </nav>
-</header>
+    <div class="header_redes">
+      <div class="redes_nav">
+        <a href="https://www.youtube.com/channel/UCrH-GU10xq1IQShqwHs29fg" target="blank">
+          <i class="fab fa-youtube fa-lg"></i>
+        </a>
+      </div>
+      <div class="redes_nav">
+        <a href="https://www.linkedin.com/company/cuatimar-construcciones" target="blank">
+          <i class="fab fa-linkedin fa-lg"></i>
+        </a>
+      </div>
+      <div class="redes_nav">
+        <a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank">
+          <i class="fas fa-at fa-lg"></i>
+        </a>
+      </div>
+      <div class="redes_nav">
+        <a href="tel:+527225443902">
+          <i class="fas fa-phone-square-alt fa-lg"></i>
+        </a>
+      </div>
+    </div>
+
+    <div class="menu_bar">
+      <img class="logo_img" src="images/Cuatimar_logo.png" alt="Logo Cuatimar">
+      <div class="welcome" style="width:80%;">
+      </div>
+      <a class="bt-menu"><i class="fas fa-bars"></i></a>
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="index" class="menu_links">Inicio</a></li>
+        <li><a href="catalogo" class="menu_links">Servicios</a></li>
+        <li><a href="contacto" class="menu_links">Contacto</a></li>
+      </ul>
+    </nav>
+  </header>
