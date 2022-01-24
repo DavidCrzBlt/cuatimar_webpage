02aba8863f8cbb0a033a089a683c00247994cd7b
David_CB <cruzdavid158@gmail.com>
2022-01-24 17:48:44 -0600

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,51 +1,21 @@
-<footer>
-     <!-- <div class="footer_title">
-          <a name="Contacto">Contacto</a>
-     </div> -->
-     <div class="footer_container">
-          <div class="footer_form">
-               <form method="post" action="php/email.php">
-                 <legend class="footer_title"><a name="Contacto">Contacto</a></legend>
-                    <br>Déjanos tus datos para contactarte<br>
-                    <br>
-                    <label for="nombre">Nombre:</label><input type="text" id="nombre" name="nombre" pattern="[A-Za-zW+]{3,}" required>
-                    <br>
-                    <label for="empresa">Empresa:</label><input type="text" id="empresa" name="empresa" required>
-                    <br>
-                    <label for="telefono">Teléfono:</label><input type="text" name="telefono" id="telefono" pattern="[0-9]{10}" placeholder="7225443902">
-                    <br>
-                    <label for="email">Dirección de email:</label>
-                    <input type="email" id="email" name="email" placeholder="me@me.com" required>
-                    <br>
-                    <label for="mensaje">Mensaje:</label>
-                    <textarea id="mensaje" name="mensaje" cols="30" rows="8"></textarea>
-                    <br>
-                    <input type="submit" id="submit" value="Contáctame">
-                    <br>
-               </form>
+<footer>     
+     <div>
+          <div>
+               <p>Dirección: Calle de los Sauces 102, Col. Los Sauces I, Toluca, Estado de México. C.P. 50210<br>Mail: <a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank">contacto@cuatimarconstrucciones.com.mx</a>
+               <br>Teléfono de oficina: <a href="tel:+527225443902">7225443902</a>
+               <br>WhatsApp: <a href="https://api.whatsapp.com/send?phone=0527295888423" target="_blank">7295888423</a>
+               <br>Quejas y sugerencias: <a href="mailto:quejas@cuatimarconstrucciones.com.mx?Subject=Quejas" target="blank">quejas@cuatimarconstrucciones.com.mx</a>
+               <br>
+               <br><a href="Aviso-privacidad">Consulta nuestro aviso de privacidad</a>
+               <br><a href="Codigo_de_etica_Cuatimar.pdf">Código de ética</a>
+               </p>
           </div>
-          <div class="footer_content">
-               <div class="contact_details">
-                    <p>Dirección: Calle de los Sauces 102, Col. Los Sauces I, Toluca, Estado de México. C.P. 50210<br>Mail: <a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank">contacto@cuatimarconstrucciones.com.mx</a>
-                    <br>Teléfono de oficina: <a href="tel:+527225443902">7225443902</a>
-                    <br>WhatsApp: <a href="https://api.whatsapp.com/send?phone=0527295888423" target="_blank">7295888423</a>
-                    <br>Quejas y sugerencias: <a href="mailto:quejas@cuatimarconstrucciones.com.mx?Subject=Quejas" target="blank">quejas@cuatimarconstrucciones.com.mx</a>
-                    <br>
-                    <br><a href="Aviso-privacidad">Consulta nuestro aviso de privacidad</a>
-                    <br><a href="Codigo_de_etica_Cuatimar.pdf">Código de ética</a>
-                    </p>
-               </div>
-               <div class="footer_redes">
-                    <div class="redes"><a href="https://www.youtube.com/channel/UCrH-GU10xq1IQShqwHs29fg" target="blank"><i class="fab fa-youtube fa-2x"></i></a></div>
-                    <div class="redes"><a href="https://www.linkedin.com/company/cuatimar-construcciones" target="blank"><i class="fab fa-linkedin fa-2x"></i></a></div>
-                    <div class="redes"><a href="https://api.whatsapp.com/send?phone=0527295888423" target="blank"><i class="fab fa-whatsapp-square fa-2x"></i></a></div>
-                    <div class="redes"><a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank"><i class="fas fa-at fa-2x"></i></a></div>
-                    <div class="redes"><a href="tel:+527225443902"><i class="fas fa-phone-square-alt fa-2x"></i></a></div>
-               </div>
+          <div class="footer_redes">
+               <div class="redes"><a href="https://www.youtube.com/channel/UCrH-GU10xq1IQShqwHs29fg" target="blank"><i class="fab fa-youtube fa-2x"></i></a></div>
+               <div class="redes"><a href="https://www.linkedin.com/company/cuatimar-construcciones" target="blank"><i class="fab fa-linkedin fa-2x"></i></a></div>
+               <div class="redes"><a href="https://api.whatsapp.com/send?phone=0527295888423" target="blank"><i class="fab fa-whatsapp-square fa-2x"></i></a></div>
+               <div class="redes"><a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank"><i class="fas fa-at fa-2x"></i></a></div>
+               <div class="redes"><a href="tel:+527225443902"><i class="fas fa-phone-square-alt fa-2x"></i></a></div>
           </div>
-     </div>
-
-     <div class="footer_final">
-          <br>Todos los derechos reservados &#169; Cuatimar Construcciones SA de CV
      </div>
 </footer>
