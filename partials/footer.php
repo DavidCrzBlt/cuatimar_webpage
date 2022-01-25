e8eb1561f4b95afb4dddb013caecb5c111f3cf6e
David_CB <cruzdavid158@gmail.com>
2022-01-24 18:06:59 -0600

    styles

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
 <footer>     
-     <div>
+     <div class="footer_container">
           <div>
                <p>Dirección: Calle de los Sauces 102, Col. Los Sauces I, Toluca, Estado de México. C.P. 50210<br>Mail: <a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank">contacto@cuatimarconstrucciones.com.mx</a>
                <br>Teléfono de oficina: <a href="tel:+527225443902">7225443902</a>
@@ -10,12 +10,12 @@
                <br><a href="Codigo_de_etica_Cuatimar.pdf">Código de ética</a>
                </p>
           </div>
-          <div class="footer_redes">
-               <div class="redes"><a href="https://www.youtube.com/channel/UCrH-GU10xq1IQShqwHs29fg" target="blank"><i class="fab fa-youtube fa-2x"></i></a></div>
-               <div class="redes"><a href="https://www.linkedin.com/company/cuatimar-construcciones" target="blank"><i class="fab fa-linkedin fa-2x"></i></a></div>
-               <div class="redes"><a href="https://api.whatsapp.com/send?phone=0527295888423" target="blank"><i class="fab fa-whatsapp-square fa-2x"></i></a></div>
-               <div class="redes"><a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank"><i class="fas fa-at fa-2x"></i></a></div>
-               <div class="redes"><a href="tel:+527225443902"><i class="fas fa-phone-square-alt fa-2x"></i></a></div>
+          <div class="header_redes">
+               <div class="icon_redes"><a href="https://www.youtube.com/channel/UCrH-GU10xq1IQShqwHs29fg" target="blank"><i class="fab fa-youtube fa-2x"></i></a></div>
+               <div class="icon_redes"><a href="https://www.linkedin.com/company/cuatimar-construcciones" target="blank"><i class="fab fa-linkedin fa-2x"></i></a></div>
+               <div class="icon_redes"><a href="https://api.whatsapp.com/send?phone=0527295888423" target="blank"><i class="fab fa-whatsapp-square fa-2x"></i></a></div>
+               <div class="icon_redes"><a href="mailto:contacto@cuatimarconstrucciones.com.mx?Subject=Cotización" target="blank"><i class="fas fa-at fa-2x"></i></a></div>
+               <div class="icon_redes"><a href="tel:+527225443902"><i class="fas fa-phone-square-alt fa-2x"></i></a></div>
           </div>
      </div>
 </footer>
