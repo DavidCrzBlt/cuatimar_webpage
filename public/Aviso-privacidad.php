7e7e5ea413a72891977b090b35a78ade188b6a3f
David_CB <cruzdavid158@gmail.com>
2022-06-28 11:51:03 -0500

    yml FILE

---
+++ public/Aviso-privacidad.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Aviso de privacidad</title>
+     <link rel="stylesheet" href="css/stylesheet.css">
+     <script src="https://kit.fontawesome.com/6c35e6223b.js" crossorigin="anonymous"></script>
+</head>
+<body id="Aviso-privacidad">
+     <?php require 'partials/header.php';  ?>
+
+     <h1>Cuatimar Construcciones SA de CV</h1>
+     <div class="bloques">
+          <h2>Aviso de Privacidad Sitio Web Integral</h2>
+                <p class="aviso">
+                     CUATIMAR CONSTRUCCIONES S.A. DE C.V. (nombre comercial: CUATIMAR) con domicilio en Calle de los Sauces 102, Los Sauces 1, Toluca, Estado de México, C. P. 50210, es el responsable del uso y protección de sus datos personales, y al respecto le informamos lo siguiente:
+                </p>
+
+           <h3>Departamento de Privacidad</h3>
+                <p class="aviso">
+                     Para atender cualquier solicitud acerca del ejercicio de sus derechos ARCO, para la revocación del consentimiento que nos haya otorgado del tratamiento de sus datos personales, para limitar el uso o divulgación de sus datos, o acerca de las finalidades para el tratamiento de sus datos personales, ponemos a su disposición nuestro Departamento de Privacidad en los siguientes medios de contacto: <br><br>Departamento de Privacidad <br>Correo electrónico: administracion@cuatimarconstrucciones.com.mx <br><br>Para poder comunicarnos con usted, necesitamos los siguientes datos personales y de contacto en su solicitud:<br>
+
+                     <ul class="priv">
+                          <li>Nombre del titular</li>
+                          <li>Carta de autorización en caso de que la solicitud la haga otra persona que no sea el titular</li>
+                          <li>Copia de identificación del titular</li>
+                          <li>Correo electrónico</li>
+                          <li>Teléfono</li>
+                     </ul>
+
+                      Después de recibir su solicitud, recibirá nuestra respuesta en un plazo máximo de veinte días hábiles por los medios de contacto que nos proporcione.
+                </p>
+           <h3>¿Para qué fines utilizaremos sus datos personales?</h3>
+                 <p class="aviso">
+                      Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:
+                      <ul class="priv">
+                           <li>Para fines de contacto</li>
+                      </ul>
+                 </p>
+           <h3>¿Qué datos personales utilizaremos para estos fines?</h3>
+                 <p class="aviso">
+                      Para llevar a cabo las finalidades descritas en el presente Aviso de Privacidad, utilizaremos los siguientes datos personales:
+                          <ul class="priv">
+                               <li>Datos de contacto</li>
+                               <li>Datos de identificación</li>
+                               <li>Datos académicos</li>
+                          </ul>
+               </p>
+            <h3>¿Cómo puede Acceder, Rectificar o Cancelar sus datos personales, u Oponerse a su uso?</h3>
+                  <p class="aviso">
+                       Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada conforme a los principios, deberes y obligaciones previstas en la normativa (Cancelación); así como oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO. Si usted desea ejercer sus derechos ARCO, lo podrá hacer contactándose con nuestro Departamento de Privacidad en los medios establecidos en el presente aviso.
+                  </p>
+             <h3>¿Cómo puede revocar su consentimiento para el uso de sus datos personales?</h3>
+                   <p class="aviso">
+                        Usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos personales. Asimismo, usted deberá considerar qué para ciertos fines, la revocación de su consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros. Para conocer el procedimiento y requisitos para la revocación de su consentimiento, usted podrá ponerse en contacto con nuestro Departamento de Privacidad a través de los medios especificados en el presente aviso.
+                   </p>
+               <h3>¿Cómo puede limitar el uso o divulgación de su información personal?</h3>
+                    <p class="aviso">
+                         Si usted desea limitar el uso o divulgación de su información personal podrá solicitarlo a nuestro Departamento de Privacidad a través de los medios especificados en el presente aviso. Adicionalmente, podemos poner a su disposición procedimientos y mecanismos específicos mediante los cuales puede limitar el uso de su información personal. Estos procedimientos y mecanismos específicos se informarán a través de los medios que utilicemos para comunicarnos con usted u otros que consideremos adecuados.
+                    </p>
+               <h3>Uso de Cookies y tecnologías de rastreo</h3>
+                    <p class="aviso">
+                         Las cookies son pedazos de información en forma de pequeños archivos que se localizan en su disco duro y son generados por nuestro sitio web. Estos archivos hacen que la interacción con nuestro sitio sea más rápida y fácil, recordando preferencias, datos de comportamiento o datos de registro en nuestro sitio. La información que contienen puede ser utilizada cuando usted visita nuestro sitio y sitios externos. Algunas páginas o correos nuestros pueden contener pequeñas imágenes invisibles llamadas “web beacons” o “pixel tags”. Los web beacons rastrean su comportamiento en una página o correo de forma similar a las cookies. Le informamos que las cookies y otras tecnologías de rastreo las utilizamos para las siguientes finalidades:
+                    </p>
+                         <ul class="priv">
+                              <li>Para el registro y análisis de comportamiento dentro de nuestro sitio con fines estadísticos y de calidad</li>
+                         </ul>
+                         <p class="aviso">
+                              Además, podemos utilizar la información para fines publicitarios y de remarketing. El remarketing es un método que muestra nuestros anuncios en sitios ajenos a éste en función de visitas pasadas a nuestro sitio. Los anuncios se pueden mostrar en forma de banners o publicaciones sugeridas. <br><br> Puede solicitar la inhabilitación del uso de cookies para publicidad, mediante la página de Network Advertising Initiative (inglés):<br><br> www.networkadvertising.org/choices/<br><br> Para inhabilitar los anuncios de Google:<br><br> www.google.com/settings/ads/?hl=es <br><br>Igualmente puede consultar las políticas de privacidad de las compañías que utilizamos para fines publicitarios y de remarketing:
+                         </p>
+                         <ul class="priv">
+                              <li>Google: www.google.com/intl/es_mx/policies/privacy/</li>
+                         </ul>
+                         <p class="aviso">
+                              Si usted prefiere deshabilitar las cookies completamente, lo puede hacer en la mayoría de los exploradores de internet. Tenga en cuenta que este sitio puede dejar de funcionar correctamente al hacerlo. En las versiones más recientes de los exploradores Mozilla Firefox, Internet Explorer y Google Chrome, la opción para deshabilitar las cookies se encuentra en la sección de “Privacidad” en las opciones de configuración de cada explorador.
+                         </p>
+               <h3>¿Cómo puede conocer los cambios a este Aviso de Privacidad?</h3>
+               <p class="aviso">
+                    El presente Aviso de Privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas.<br><br> Los cambios al Aviso de Privacidad se informarán a través de nuestro sitio web en la siguiente dirección: www.cuatimar.com.mx
+               </p>
+     </div>
+
+     <?php require 'partials/footer.php'; ?>
+
+     <script src="js/app.js"></script>
+</body>
+</html>
